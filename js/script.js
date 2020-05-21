df6674848c0d7f0bd225cfbf1881298e188f0e59
lindam <linda.minotti2@gmail.com>
2020-05-21 17:51:00 +0200

    Create funzioni per i bottoni: genera e annulla.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
-document.getElementById('ticket_name').innerHTML = 'Prova layout';
-document.getElementById('ticket_code').innerHTML = 'Prova layout';
-document.getElementById('ticket_offer').innerHTML = 'Prova layout';
-document.getElementById('ticket_price').innerHTML = 'Prova layout';
-document.getElementById('ticket_carriage').innerHTML = 'Prova layout';
+// Bottoni del form
+var buttonGenera = document.getElementById('button_genera');
+var buttonAnnulla = document.getElementById('button_annulla');
+
+// Elementi del form
+var formName = document.getElementById('form_name');
+var formKm = document.getElementById('form_km');
+var formEta = document.getElementById('form_eta')
+
+
+// Elementi biglietto
+var ticketName = document.getElementById('ticket_name');
+
+// Evento quando si clicca Genera
+buttonGenera.addEventListener('click',
+  function(){
+    ticketName.innerHTML = formName.value
+  }
+)
+
+//Evento quando si clicca annulla
+buttonAnnulla.addEventListener('click',
+  function(){
+    nome.value = '';
+    ticketName.innerHTML ='';
+
+  }
+)
