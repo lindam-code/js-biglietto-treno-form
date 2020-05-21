bee7441e1b1b3e7e00f78eb5850612cc99abd061
lindam <linda.minotti2@gmail.com>
2020-05-21 16:38:34 +0200

    Preparato layout sezione biglietto.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
-
+document.getElementById('ticket_name').innerHTML = 'Prova layout';
+document.getElementById('ticket_code').innerHTML = 'Prova layout';
+document.getElementById('ticket_offer').innerHTML = 'Prova layout';
+document.getElementById('ticket_price').innerHTML = 'Prova layout';
+document.getElementById('ticket_carriage').innerHTML = 'Prova layout';
